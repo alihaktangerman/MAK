089c683a34812618f805e44b0e2e22bd8585d29e
Ali Haktan <107588410+alihaktangerman@users.noreply.github.com>
2023-01-04 19:49:46 +0300

    Update crypto.jl

--- crypto.jl
+++ crypto.jl
@@ -14,13 +14,13 @@ ZZq = Int/q;
 #convention
 d = Normal(.0,1.)
 #key generation
-x = [ZZq(rand(Int)) for i=1:n] #constructing n-vector
-A = [ZZq(rand(Int)) for i=1:m, j=1:n]; #constructing (mxn)-matrix
-e = ZZq.(Int.(round.(rand(d,m))))
+x = rand(ZZq,n) #constructing n-vector
+A = rand(ZZq,m,n); #constructing (mxn)-matrix
+e = ZZq.(round.(rand(d,m)))
 y = A*x+e
 #encryption
 b = ZZq(1)
-w = permutedims([ZZq(rand(Bool)) for i=1:m])
+w = ZZq.(rand(Bool,1,m))
 c = w*[A y]
 c[end] += b*Int(floor(q/2))
 #decryption
